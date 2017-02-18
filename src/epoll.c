88656f75f0e138990fb8010a67ed73b56fbe795f
Jan Kokemüller <jan.kokemueller@gmail.com>
2017-02-18 20:00:37 +0100

    invert the EPOLLOUT/EPOLLIN conditionals for EPOLL_CTL_MOD
    Hopefully they are correct now.

--- src/epoll.c
+++ src/epoll.c
@@ -104,10 +104,8 @@ epoll_ctl(int fd, int op, int fd2, struct epoll_event *ev)
 			// Existing events will be modified.
 			ret = epoll_ctl_add(fd, fd2, ev);
 
-			// TODO: This probably doesn't do what it should do.
-			// The parenthesis were missing here and due to C
-			// precedence rules this conditional was always false.
-		} else if ((ev->events & EPOLLOUT) == 0) {
+			// TODO: make sure this is correct
+		} else if (ev->events & EPOLLOUT) {
 			// Is it OK to assume this?
 			ret = epoll_kevent_set(
 			    fd, fd2, EVFILT_WRITE, EV_DELETE, 0, 0, 0);
@@ -116,7 +114,7 @@ epoll_ctl(int fd, int op, int fd2, struct epoll_event *ev)
 			ret = 0;
 
 			// TODO: same as above
-		} else if ((ev->events & EPOLLIN) == 0) {
+		} else if (ev->events & EPOLLIN) {
 			// Is it OK to assume this?
 			ret = epoll_kevent_set(
 			    fd, fd2, EVFILT_READ, EV_DELETE, 0, 0, 0);
