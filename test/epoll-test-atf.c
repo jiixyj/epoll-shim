8eb240c0a1b246e9453a23673c79c4bce80b8904
Jan Kokemüller <jan.kokemueller@gmail.com>
2019-11-10 19:04:15 +0100

    use correct operator for testing equality

--- test/epoll-test-atf.c
+++ test/epoll-test-atf.c
@@ -24,7 +24,7 @@ ATF_TC_BODY(epoll__simple, tc)
 	ATF_REQUIRE((fd = epoll_create1(EPOLL_CLOEXEC)) >= 0);
 	ATF_REQUIRE(close(fd) >= 0);
 
-	ATF_REQUIRE(lfd = lowest_fd());
+	ATF_REQUIRE(lfd == lowest_fd());
 }
 
 ATF_TC_WITHOUT_HEAD(epoll__invalid_op);
@@ -37,7 +37,7 @@ ATF_TC_BODY(epoll__invalid_op, tc)
 	ATF_REQUIRE_ERRNO(EINVAL, epoll_ctl(fd, 3, 5, NULL));
 	ATF_REQUIRE(close(fd) >= 0);
 
-	ATF_REQUIRE(lfd = lowest_fd());
+	ATF_REQUIRE(lfd == lowest_fd());
 }
 
 ATF_TP_ADD_TCS(tp)
