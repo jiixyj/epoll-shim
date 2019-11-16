6889f39147c6e902de46023244a72de45dcff245
Jan Kokemüller <jan.kokemueller@gmail.com>
2019-11-16 15:22:32 +0100

    create separate file for microatf related tests

--- test/atf-test.c
+++ test/atf-test.c
@@ -1,23 +1,10 @@
 #include <atf-c.h>
 
-#include <sys/epoll.h>
-
-#include <sys/event.h>
-
 #include <errno.h>
 #include <signal.h>
 #include <stdlib.h>
 
 #include <dirent.h>
-
-static int
-lowest_fd(void)
-{
-	int kq = kqueue();
-	ATF_REQUIRE(kq >= 0);
-	close(kq);
-	return kq;
-}
 
 ATF_TC_WITHOUT_HEAD(atf__environment);
 ATF_TC_BODY(atf__environment, tc)
@@ -83,31 +70,6 @@ ATF_TC_BODY(atf__signal, tc)
 	kill(getpid(), SIGHUP);
 }
 
-ATF_TC_WITHOUT_HEAD(epoll__simple);
-ATF_TC_BODY(epoll__simple, tc)
-{
-	int lfd = lowest_fd();
-	int fd;
-
-	ATF_REQUIRE((fd = epoll_create1(EPOLL_CLOEXEC)) >= 0);
-	ATF_REQUIRE(close(fd) >= 0);
-
-	ATF_REQUIRE_EQ(lfd, lowest_fd());
-}
-
-ATF_TC_WITHOUT_HEAD(epoll__invalid_op);
-ATF_TC_BODY(epoll__invalid_op, tc)
-{
-	int lfd = lowest_fd();
-	int fd;
-
-	ATF_REQUIRE((fd = epoll_create1(EPOLL_CLOEXEC)) >= 0);
-	ATF_REQUIRE_ERRNO(EINVAL, epoll_ctl(fd, 3, 5, NULL) < 0);
-	ATF_REQUIRE(close(fd) >= 0);
-
-	ATF_REQUIRE_EQ(lfd, lowest_fd());
-}
-
 ATF_TP_ADD_TCS(tp)
 {
 	ATF_TP_ADD_TC(tp, atf__environment);
@@ -115,8 +77,6 @@ ATF_TP_ADD_TCS(tp)
 	ATF_TP_ADD_TC(tp, atf__checkfail);
 	ATF_TP_ADD_TC(tp, atf__exit_code);
 	ATF_TP_ADD_TC(tp, atf__signal);
-	ATF_TP_ADD_TC(tp, epoll__simple);
-	ATF_TP_ADD_TC(tp, epoll__invalid_op);
 
 	return atf_no_error();
 }
