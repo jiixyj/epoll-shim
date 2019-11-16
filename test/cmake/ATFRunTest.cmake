6c27ecb618d9797a297c51effa7cc9b7494ff7cc
Jan Kokemüller <jan.kokemueller@gmail.com>
2019-11-16 12:15:40 +0100

    convert perf-many-fds.c test to ATF

--- test/cmake/ATFRunTest.cmake
+++ test/cmake/ATFRunTest.cmake
@@ -10,11 +10,11 @@ set(_wd "${BINARY_DIR}/${TEST_FOLDER_NAME}")
 
 execute_process(COMMAND "${CMAKE_COMMAND}" -E remove_directory "${_wd}")
 execute_process(COMMAND "${CMAKE_COMMAND}" -E   make_directory "${_wd}/work")
+execute_process(COMMAND "${CMAKE_COMMAND}" -E            touch "${_wd}/result")
 
-set(ENV_PROGRAM "${CMAKE_COMMAND}" -E env)
 execute_process(
   COMMAND
-    ${ENV_PROGRAM} --unset=LANG --unset=LC_ALL --unset=LC_COLLATE
+    "${CMAKE_COMMAND}" -E env --unset=LANG --unset=LC_ALL --unset=LC_COLLATE
     --unset=LC_CTYPE --unset=LC_MESSAGES --unset=LC_MONETARY --unset=LC_NUMERIC
     --unset=LC_TIME "HOME=${_wd}/work" "TMPDIR=${_wd}/work" "TZ=UTC"
     "__RUNNING_INSIDE_ATF_RUN=internal-yes-value"
@@ -30,13 +30,11 @@ file(STRINGS "${_wd}/stderr" _stderr_lines)
 execute_process(COMMAND "${CMAKE_COMMAND}" -E remove_directory "${_wd}")
 
 list(LENGTH _result_line _result_line_length)
-if(NOT _result_line_length EQUAL 1)
-  message(FATAL_ERROR "")
+if(_result_line_length GREATER 1)
+  message(FATAL_ERROR "Result must not consist of multiple lines!")
 endif()
 
-
 message(STATUS "result: ${_result}, ${_result_line}")
-
 
 #[[
 "expected_death", -1, &formatted
@@ -98,6 +96,8 @@ elseif(_result_line MATCHES "^expected_signal\\((.*)\\): (.*)$")
   endif()
 
 else()
-  message(FATAL_ERROR "")
+  message(
+    FATAL_ERROR
+      "Unexpected result: \"${_result_line}\", process exited with: ${_result}")
 
 endif()
--- test/perf-many-fds.c
+++ test/perf-many-fds.c
@@ -1,43 +1,44 @@
+#include <atf-c.h>
+
 #include <sys/eventfd.h>
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 
-#define REQUIRE(x)                                                            \
-	do {                                                                  \
-		if (!(x)) {                                                   \
-			fprintf(stderr, "REQUIRE in line %d failed.\n",       \
-			    __LINE__);                                        \
-			exit(EXIT_FAILURE);                                   \
-		}                                                             \
-	} while (0)
-
 #define NR_EVENTFDS (20000)
 
-int
-main()
+/*
+ * Those eventfd operations must not take longer than 15s! This was previously
+ * the case with the old, naive list data structure used to manage eventfds.
+ */
+
+ATF_TC(perf_many_fds__perf);
+ATF_TC_HEAD(perf_many_fds__perf, tc)
 {
-	struct timespec time1;
-	struct timespec time2;
-
-	REQUIRE(clock_gettime(CLOCK_MONOTONIC, &time1) == 0);
-
+	atf_tc_set_md_var(tc, "timeout", "15");
+}
+ATF_TC_BODY(perf_many_fds__perf, tc)
+{
 	int *eventfds = malloc(NR_EVENTFDS * sizeof(int));
-	REQUIRE(eventfds);
+	ATF_REQUIRE(eventfds);
 
 	for (long i = 0; i < NR_EVENTFDS; ++i) {
 		eventfds[i] = eventfd(0, EFD_CLOEXEC | EFD_NONBLOCK);
-		REQUIRE(eventfds[i] >= 0);
+		ATF_REQUIRE(eventfds[i] >= 0);
 	}
 
 	for (long i = 0; i < 2000000; ++i) {
-		REQUIRE(eventfd_write(eventfds[0], 1) == 0);
+		ATF_REQUIRE(eventfd_write(eventfds[0], 1) == 0);
 		if (i % 10000 == 0) {
 			fprintf(stderr, ".");
 		}
 	}
+}
 
-	REQUIRE(clock_gettime(CLOCK_MONOTONIC, &time2) == 0);
-	REQUIRE(time2.tv_sec - time1.tv_sec < 15);
+ATF_TP_ADD_TCS(tp)
+{
+	ATF_TP_ADD_TC(tp, perf_many_fds__perf);
+
+	return atf_no_error();
 }
