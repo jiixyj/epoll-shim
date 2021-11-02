46a8f5d2eb4874e5eeebcb7365e118433418522a
Jan Kokemüller <jan.kokemueller@gmail.com>
2021-11-02 21:42:25 +0100

    put elf wrappers into own .so

--- src/wrap.c
+++ src/wrap.c
@@ -56,32 +56,12 @@ wrap_initialize(void)
 	errno = oe;
 }
 
-#define WRAPPERS
-
-#ifdef WRAPPERS
-EPOLL_SHIM_EXPORT
-ssize_t
-read(int fd, void *buf, size_t nbytes)
-{
-	return epoll_shim_read(fd, buf, nbytes);
-}
-#endif
-
 ssize_t
 real_read(int fd, void *buf, size_t nbytes)
 {
 	wrap_initialize();
 	return wrap.real_read(fd, buf, nbytes);
 }
-
-#ifdef WRAPPERS
-EPOLL_SHIM_EXPORT
-ssize_t
-write(int fd, void const *buf, size_t nbytes)
-{
-	return epoll_shim_write(fd, buf, nbytes);
-}
-#endif
 
 ssize_t
 real_write(int fd, void const *buf, size_t nbytes)
@@ -90,15 +70,6 @@ real_write(int fd, void const *buf, size_t nbytes)
 	return wrap.real_write(fd, buf, nbytes);
 }
 
-#ifdef WRAPPERS
-EPOLL_SHIM_EXPORT
-int
-close(int fd)
-{
-	return epoll_shim_close(fd);
-}
-#endif
-
 int
 real_close(int fd)
 {
@@ -106,36 +77,12 @@ real_close(int fd)
 	return wrap.real_close(fd);
 }
 
-#ifdef WRAPPERS
-EPOLL_SHIM_EXPORT
-int
-poll(struct pollfd fds[], nfds_t nfds, int timeout)
-{
-	return epoll_shim_poll(fds, nfds, timeout);
-}
-#endif
-
 int
 real_poll(struct pollfd fds[], nfds_t nfds, int timeout)
 {
 	wrap_initialize();
 	return wrap.real_poll(fds, nfds, timeout);
 }
-
-#ifdef WRAPPERS
-EPOLL_SHIM_EXPORT
-int
-#ifdef __NetBSD__
-__pollts50
-#else
-ppoll
-#endif
-    (struct pollfd fds[], nfds_t nfds, struct timespec const *restrict timeout,
-	sigset_t const *restrict newsigmask)
-{
-	return epoll_shim_ppoll(fds, nfds, timeout, newsigmask);
-}
-#endif
 
 int
 real_ppoll(struct pollfd fds[], nfds_t nfds,
@@ -149,22 +96,6 @@ real_ppoll(struct pollfd fds[], nfds_t nfds,
 	return wrap.real_ppoll(fds, nfds, timeout, newsigmask);
 #endif
 }
-
-#ifdef WRAPPERS
-EPOLL_SHIM_EXPORT
-int
-fcntl(int fd, int cmd, ...)
-{
-	va_list ap;
-
-	va_start(ap, cmd);
-	void *arg = va_arg(ap, void *);
-	int rv = epoll_shim_fcntl(fd, cmd, arg);
-	va_end(ap);
-
-	return rv;
-}
-#endif
 
 int
 real_fcntl(int fd, int cmd, ...)
