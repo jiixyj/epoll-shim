8f0a1d9a2468fd7964ea3711d22b8ceaf12c0bd7
Jan Kokemüller <jan.kokemueller@gmail.com>
2022-06-06 20:16:35 +0200

    add non-variadic wrapper macros for ANSI C support

--- include/epoll-shim/detail/poll.h
+++ include/epoll-shim/detail/poll.h
@@ -5,10 +5,19 @@
 #include <signal.h>
 
 extern int epoll_shim_poll(struct pollfd *, nfds_t, int);
+#ifdef EPOLL_SHIM_NO_VARIADICS
+#define poll(fds, nfds, timeout) epoll_shim_poll((fds), (nfds), (timeout))
+#else
 #define poll(...) epoll_shim_poll(__VA_ARGS__)
+#endif
 
 extern int epoll_shim_ppoll(struct pollfd *, nfds_t, struct timespec const *,
     sigset_t const *);
+#ifdef EPOLL_SHIM_NO_VARIADICS
+#define ppoll(fds, nfds, tmo_p, sigmask) \
+	epoll_shim_ppoll((fds), (nfds), (tmo_p), (sigmask))
+#else
 #define ppoll(...) epoll_shim_ppoll(__VA_ARGS__)
+#endif
 
 #endif
--- include/epoll-shim/detail/read.h
+++ include/epoll-shim/detail/read.h
@@ -4,6 +4,10 @@
 #include <unistd.h>
 
 extern ssize_t epoll_shim_read(int, void *, size_t);
+#ifdef EPOLL_SHIM_NO_VARIADICS
+#define read(fd, buf, count) epoll_shim_read((fd), (buf), (count))
+#else
 #define read(...) epoll_shim_read(__VA_ARGS__)
+#endif
 
 #endif
