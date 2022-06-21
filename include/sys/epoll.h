9a897da1930503450f8363a5a84c5d80e809d9c8
Torrekie <me@torrekie.dev>
2022-06-21 17:32:07 +0800

    Revert "packed struct for all LP64 systems"
    This reverts commit 5bd3e940d15e6076d379b0a932a0b39a30a5b347.

--- include/sys/epoll.h
+++ include/sys/epoll.h
@@ -47,9 +47,7 @@ struct epoll_event {
 	uint32_t events;
 	epoll_data_t data;
 }
-#if (defined(_LP64) || defined(__LP64__)) || \
-	(defined(__x86_64__) || defined(__x86_64) || defined(__amd64) || defined(__amd64__)) || \
-	(defined(__arm64__) || defined(__arm64) || defined(__aarch64__))
+#ifdef __x86_64__
 __attribute__((__packed__))
 #endif
 ;
